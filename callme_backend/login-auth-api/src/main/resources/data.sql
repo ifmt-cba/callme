0f57168662f704b5c1ea824c099f6837879bddce
AnthonyGoc <anthonygabrieloliveiracruz@gmail.com>
2025-04-13 18:35:57 -0400

    Criando User controlle e criando o metodo para colocar adicionar um RT no banco de dados, tambem foi corrigido alguns bugs que impossibilitavam a insercao de entidades no banco

--- callme_backend/login-auth-api/src/main/resources/data.sql
+++ callme_backend/login-auth-api/src/main/resources/data.sql
@@ -1,3 +1,2 @@
 INSERT INTO tb_roles (role_id, name) VALUES (1, 'ADMIN') ON CONFLICT DO NOTHING;
-INSERT INTO tb_roles (role_id, name) VALUES (2, 'USER') ON CONFLICT DO NOTHING;
-
+INSERT INTO tb_roles (role_id, name) VALUES (2, 'RT') ON CONFLICT DO NOTHING;
