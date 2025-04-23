449a65a398d6f22814ba4b9bc243884d1c3ede11
AnthonyGoc <anthonygabrieloliveiracruz@gmail.com>
2025-04-22 22:51:52 -0400

    Criando metodo onde o usuario criador do chamado e O AMDIN tem a permisao de deletar-los

--- callme_backend/login-auth-api/src/main/java/com/example/login_auth_api/controller/ChamadoController.java
+++ callme_backend/login-auth-api/src/main/java/com/example/login_auth_api/controller/ChamadoController.java
@@ -1,14 +1,15 @@
 package com.example.login_auth_api.controller;
 
 import com.example.login_auth_api.domain.user.Chamado;
+import com.example.login_auth_api.domain.user.Role;
 import com.example.login_auth_api.dto.CreateChamadoDto;
 import com.example.login_auth_api.repositories.ChamadoRepository;
 import com.example.login_auth_api.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.UUID;
 
@@ -37,6 +38,26 @@ public class ChamadoController {
 
            chamadoRepository.save(chamado);
            return ResponseEntity.ok().build();
+    }
+    @DeleteMapping("/chamados/{id}")
+    public ResponseEntity<Void> deletechamado(@PathVariable ("id") Long chamadoId,
+        JwtAuthenticationToken token){
+        var user = userRepository.findById(UUID.fromString(token.getName()));
+        var chamado = chamadoRepository.findById(chamadoId)
+                        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        var isAdmin = user.get().getRoles().stream().anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
+
+        if (isAdmin || chamado.getUser().getUserid().equals(UUID.fromString(token.getName()))) {
+            chamadoRepository.deleteById(chamadoId);
+
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+
+
+        return ResponseEntity.ok().build();
+
     }
 
 }
