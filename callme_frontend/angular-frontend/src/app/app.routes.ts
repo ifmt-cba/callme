1f67145dad571fed34a6d77d48cba0e4f264837d
AnthonyGoc <anthonygabrieloliveiracruz@gmail.com>
2025-04-23 23:47:27 -0400

    Criando o caminho login para usuarios

--- callme_frontend/angular-frontend/src/app/app.routes.ts
+++ callme_frontend/angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {DefaultLoginLayoutComponent} from "./components/default-login-layout/default-login-layout.component";
+import {LoginComponent} from "./pages/login/login.component";
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
