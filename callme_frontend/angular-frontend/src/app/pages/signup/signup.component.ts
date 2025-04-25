4a55e7b0a20d80b75141feb320f06cec109ea0b8
AnthonyGoc <anthonygabrieloliveiracruz@gmail.com>
2025-04-25 00:26:20 -0400

    criando tela de registro

---
+++ callme_frontend/angular-frontend/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import {DefaultLoginLayoutComponent} from "../../components/default-login-layout/default-login-layout.component";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {PrimaryInputComponent} from "../../components/primary-input/primary-input.component";
+import {Router} from "@angular/router";
+import {LoginService} from "../../services/login.service";
+import {ToastrService} from "ngx-toastr";
+
+interface SignupForm {
+  username: FormControl,
+  password: FormControl,
+
+}
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
+  providers: [
+    LoginService
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+  signupForm!: FormGroup<SignupForm>;
+
+  constructor(private router: Router,
+  private loginService: LoginService, private toastService: ToastrService
+  ) {
+
+    this.signupForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    })
+  }
+
+  submit() {
+   this.loginService.login(this.signupForm.value.username, this.signupForm.value.password).subscribe({
+     next: () => this.toastService.success("Login successfull"),
+     error: () => this.toastService.error("Senha ou usuario incorretos "),
+   })
+  }
+
+  navigate(){
+    this.router.navigate(["login"]);
+  }
+
+}
