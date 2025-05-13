9c5a1becaa18ebbb7e2c0798a169d21f572292a6
freitaszLe <leticia.freitasz712@gmail.com>
2025-05-13 19:14:30 -0400

    Vizualizacao de anexos no email funcionando

--- callme_backend/login-auth-api/src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
+++ callme_backend/login-auth-api/src/main/java/com/example/login_auth_api/infra/security/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
                         requestMatchers(HttpMethod.POST, "/login").permitAll().
                         requestMatchers(HttpMethod.POST, "/email").permitAll().
                         requestMatchers(HttpMethod.GET, "/email/receber").permitAll().
+                        requestMatchers(HttpMethod.GET, "/anexos/visualizar/{id}").permitAll().
                         requestMatchers(HttpMethod.POST, "/users").permitAll().
                         anyRequest().authenticated())
                 .csrf(csrf ->csrf.disable())
