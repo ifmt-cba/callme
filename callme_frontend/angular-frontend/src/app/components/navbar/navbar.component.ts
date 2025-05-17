6b3ef38284b345e109898bead3fc06fd210f15ce
AnthonyGoc <anthonygabrieloliveiracruz@gmail.com>
2025-05-17 02:21:35 -0400

    Arrunando a NavBar

--- callme_frontend/angular-frontend/src/app/components/navbar/navbar.component.ts
+++ callme_frontend/angular-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {RouterLink} from "@angular/router";
 import {navbarData} from "./nav-data";
+interface SideNavToggle{
+  screenWidth: number;
+  collapsed: boolean;
+}
 
 @Component({
   selector: 'app-navbar',
@@ -18,7 +22,20 @@ import {navbarData} from "./nav-data";
 })
 export class NavbarComponent {
 
-  collapsed = true;
+  @Output()onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth =0;
   navData = navbarData
+
+  ToggleCollapsed() {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed:this.collapsed, screenWidth: this.screenWidth});
+  }
+
+  CloseSidenav() {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed:this.collapsed, screenWidth: this.screenWidth});
+
+  }
 
 }
