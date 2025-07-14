c2389736eebf8355696ea98f20f870e0cfa03361
freitaszLe <leticia.freitasz712@gmail.com>
2025-07-14 12:44:04 -0400

    fix:vizualizacao-dos-comentarios-do-tecnico

--- callme_frontend/angular-frontend/src/app/models/acompanhamento.model.ts
+++ callme_frontend/angular-frontend/src/app/models/acompanhamento.model.ts
@@ -1,4 +1,12 @@
+export interface ComentarioDTO {
+  texto: string;
+  dataCriacao: string;
+  autorUsername: string;
+}
+
+// 2. Adicione a lista de comentários à sua interface principal
 export interface Acompanhamento {
   status: string;
   dataAbertura: string;
+  comentarios?: ComentarioDTO[]; // Propriedade opcional para a lista de comentários
 }
